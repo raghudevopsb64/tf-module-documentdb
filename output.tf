c1dbd8d009ce0f8445f1ae826f76fdeca6199ec7
Raghu K <raghuk.vit@gmail.com>
2022-06-15 07:56:15 +0530

    Update roboshop-infra/all-modules/tf-module-documentdb/

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "DOCDB_ENDPOINT" {
   value = aws_docdb_cluster.main.endpoint
 }
+
+output "DOCDB_USERNAME" {
+  value = aws_docdb_cluster.main.master_username
+}
+
+output "DOCDB_PASSWORD" {
+  value = aws_docdb_cluster.main.master_password
+}
