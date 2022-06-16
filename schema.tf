bfeef17a0b9e02e951511289084e24e5ca57af19
Raghu K <raghuk.vit@gmail.com>
2022-06-16 08:35:53 +0530

    Update roboshop-infra/all-modules/tf-module-documentdb/

--- schema.tf
+++ schema.tf
@@ -6,8 +6,7 @@ cd /tmp
 curl -L -O https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
 unzip /tmp/mongodb.zip
 cd mongodb-main
-mongo --ssl --host ${aws_docdb_cluster.main.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password <insertYourPassword>
-mongo --host ${aws_docdb_cluster.main.endpoint} -u${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USERNAME"]} -p${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASSWORD"]} <${var.DB_NAME}.js
+mongo --ssl --host ${aws_docdb_cluster.main.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USERNAME"]} --password ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASSWORD"]} <${var.DB_NAME}.js
 EOT
   }
 }
